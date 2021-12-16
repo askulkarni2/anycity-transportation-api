e0fa583d64646b164c40411137b93a5af3abc0bb
Apoorva Kulkarni <kuapoorv@amazon.com>
2021-12-15 16:12:35 -0800

    vpc and database created

--- lib/anycity-transportation-api-stack.ts
+++ lib/anycity-transportation-api-stack.ts
@@ -1,16 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib/core';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
 
 export class AnycityTransportationApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, 'vpc');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AnycityTransportationApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // RDS
+    const database = new rds.ServerlessCluster(this, 'database', {
+      engine: rds.DatabaseClusterEngine.AURORA,
+      vpc,
+      enableDataApi: true,
+    });
+
+    // Lambda
+
+    // API Gateway
   }
 }
